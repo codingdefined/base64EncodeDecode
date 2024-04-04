5e1a709072a97d94902e9145b2321b733cc7027c
codingdefined <codingdefined@gmail.com>
2024-04-04 16:43:32 +0530

    Changes done

--- src/app/base64decode/base64decode.component.ts
+++ src/app/base64decode/base64decode.component.ts
@@ -1,12 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule, NgIf } from '@angular/common';
+import { JsonFormatterDirective } from '../json-formatter.directive';
 
 @Component({
   selector: 'app-base64decode',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgIf, CommonModule, JsonFormatterDirective],
   templateUrl: './base64decode.component.html',
   styleUrl: './base64decode.component.scss'
 })
 export class Base64decodeComponent {
+  textToDecode: string = '';
+  resultInString: string = '';
+  isChecked: boolean = true;
+  resultedString: string = '0';
+  formattedString: string = '';
 
+  ngOnInit() {
+    
+  }
+
+  decode() {
+    try {
+      this.resultInString = atob(this.textToDecode)
+    } catch (error) {
+      this.resultInString = "There is error in your input"
+    }
+    
+    this.getInputType();
+  }
+
+  inputChange(event: any) {
+    if(this.isChecked) {
+      try {
+        this.resultInString = atob(event.target.value)
+      } catch (error) {
+        this.resultInString = "There is error in your input"
+      }
+      
+      this.getInputType();
+    }
+  }
+
+  getInputType() {
+    this.resultedString = '0';
+    try {
+      let a = JSON.parse(this.resultInString);
+      this.resultedString = '1';
+    } catch(e) {
+     let parser = new DOMParser;
+     var xmlDoc = parser.parseFromString(this.resultInString,"application/xml");
+        if(xmlDoc.documentElement.nodeName == "parsererror")
+          this.resultedString = '0';
+        else 
+          this.resultedString = '2';
+    } 
+  }
+
+  formatJSON() {
+    if(this.resultedString === '1'){
+      this.formattedString = JSON.stringify(JSON.parse(this.resultInString), null, 4);
+    }
+  }
 }
--- src/app/base64encode/base64encode.component.ts
+++ src/app/base64encode/base64encode.component.ts
@@ -9,14 +9,32 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './base64encode.component.scss'
 })
 export class Base64encodeComponent implements OnInit {
-  dataInBase64: string = '';
-  resultInString: string = '';
+  textToEncode: string = '';
+  resultInBase64: string = '';
+  isChecked: boolean = true;
 
   ngOnInit() {
     
   }
 
   encode() {
-    this.resultInString = btoa(this.dataInBase64)
+    try {
+      this.resultInBase64 = btoa(this.textToEncode)
+    } catch (error) {
+      this.resultInBase64 = "Could not create base64 from input"
+    }
+    
+  }
+
+  inputChange(event: any) {
+    if(this.isChecked)
+    {
+      try {
+        this.resultInBase64 = btoa(event.target.value)
+      } catch (error) {
+        this.resultInBase64 = "Could not create base64 from input"
+      }
+    }
+      
   }
 }
